e386db2a73cab63a8fe2a35a31adfc7df64652bd
starleouf <yannick.varinot@gmail.com>
2017-03-31 12:07:05 +0200

    Add files via upload

---
+++ pwmsender.ino
@@ -0,0 +1,21 @@
+int analogsender = 11;
+int val = 0;
+
+
+void setup() {
+  pinMode (analogsender,OUTPUT);
+  Serial.begin(9600);
+    
+
+}
+
+void loop() {
+  val++;
+  if(val>=255){
+    val=0;
+  }
+  Serial.println(val);
+  analogWrite(analogsender,val);
+  delay (100);
+  
+}
